fb0ba3ae962d75270117e18dc97fd5f4ac23f43b
IamJustBread <38608134+IamJustBread@users.noreply.github.com>
2023-12-25 21:31:13 +0300

    Initial commit

---
+++ go.mod
@@ -0,0 +1,9 @@
+module YDDY_HDD
+
+go 1.22rc1
+
+require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/go-sql-driver/mysql v1.7.1
+	github.com/joho/godotenv v1.5.1
+)
---
+++ main.go
@@ -0,0 +1,150 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/json"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/joho/godotenv"
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+)
+
+var db *sql.DB
+
+var tpl = template.Must(template.ParseFiles("index.html"))
+
+type APIResponse struct {
+	Message string `json:"message"`
+}
+
+type APIResponseWithData struct {
+	Message string      `json:"message"`
+	Data    interface{} `json:"data"`
+}
+
+type DatabaseConfig struct {
+	User string
+	Pass string
+	Name string
+	Host string
+}
+
+type ContentType struct {
+	ID              int     `json:"id"`
+	SubstanceName   string  `json:"substance_name"`
+	Density         float64 `json:"density"`
+	DetonationForce float64 `json:"detonation_force"`
+	TntEquivalent   float64 `json:"tnt_equivalent"`
+}
+
+func loadDatabaseConfig() DatabaseConfig {
+	return DatabaseConfig{
+		User: os.Getenv("DB_USER"),
+		Pass: os.Getenv("DB_PASS"),
+		Name: os.Getenv("DB_NAME"),
+		Host: os.Getenv("DB_HOST"),
+	}
+}
+
+func handleError(w http.ResponseWriter, err error, status int, message string) {
+	log.Printf("[%d] %s: %v", status, message, err)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	json.NewEncoder(w).Encode(APIResponse{Message: message})
+}
+
+func initDB() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Print("Error loading .env file")
+	}
+	config := loadDatabaseConfig()
+	dbURI := config.User + ":" + config.Pass + "@tcp(" + config.Host + ")/" + config.Name + "?charset=utf8&parseTime=True"
+	db, err = sql.Open("mysql", dbURI)
+	if err != nil {
+		handleError(nil, err, http.StatusInternalServerError, "Error connecting to database")
+	}
+	err = db.Ping()
+	if err != nil {
+		handleError(nil, err, http.StatusInternalServerError, "Error connecting to database")
+	}
+
+	log.Print("Connected to database")
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+
+	err := tpl.Execute(w, nil)
+	if err != nil {
+		handleError(w, err, http.StatusInternalServerError, "Error executing template")
+	}
+}
+
+func apiHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {
+	case "/api/contenttypes":
+		// Обработка запроса на получение списка типов содержимого
+		contentTypes, err := getContentTypesFromDB()
+		if err != nil {
+			handleError(w, err, http.StatusInternalServerError, "Error getting content types from database")
+			return
+		}
+
+		// Отправка списка типов содержимого в формате JSON
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		err = json.NewEncoder(w).Encode(contentTypes)
+		if err != nil {
+			handleError(w, err, http.StatusInternalServerError, "Error encoding JSON")
+			return
+		}
+	default:
+		handleError(w, nil, http.StatusNotFound, "Not found")
+	}
+}
+
+func getContentTypesFromDB() ([]ContentType, error) {
+	rows, err := db.Query("SELECT id, substance_name, density, detonation_force, tnt_equivalent FROM explosives")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var contentTypes []ContentType
+	for rows.Next() {
+		var contentType ContentType
+		if err := rows.Scan(&contentType.ID, &contentType.SubstanceName, &contentType.Density, &contentType.DetonationForce, &contentType.TntEquivalent); err != nil {
+			return nil, err
+		}
+		contentTypes = append(contentTypes, contentType)
+	}
+
+	return contentTypes, nil
+}
+
+func main() {
+	initDB()
+	err := godotenv.Load()
+	if err != nil {
+		log.Print("Error loading .env file")
+	}
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	mux := http.NewServeMux()
+
+	mux.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+	mux.HandleFunc("/", indexHandler)
+	mux.HandleFunc("/api", apiHandler)
+	mux.HandleFunc("/api/contenttypes", apiHandler)
+	mux.HandleFunc("/api/calculate", apiHandler)
+
+	err = http.ListenAndServe(":"+port, mux)
+	if err != nil {
+		handleError(nil, err, http.StatusInternalServerError, "Error starting server")
+	}
+}
